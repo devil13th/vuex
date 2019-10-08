75f12c4560c4dac6fdd709868bcdbb6b6579485f
十三妖 <181907667@qq.com>
2019-10-08 23:35:36 +0800

    ..

--- src/components/vuextest/componentF/ComponentF.vue
+++ src/components/vuextest/componentF/ComponentF.vue
@@ -5,7 +5,21 @@
         {{name2}}<br/>
         {{name3}}<br/>
         <button @click="modifyNameAction">修改名字</button>
+        <a-divider/>
+        调动其他模块的state:
         {{this.$store.state.moduleC.text}}
+
+        <a-divider/>
+        调用本模块的state：<br/> userName:{{this.state.user.userName}}  age:{{this.state.user.age}}
+        <a-divider/>
+        <button @click="callMutations01">调用模块中mutations中的方法 方式1</button>
+        <button @click="callMutations02">调用模块中mutations中的方法 方式2</button>
+        <a-divider/>
+        <a-spin :spinning="state.loading">
+          <button @click="callAction01">调用模块中Actions中的方法 方式1</button>
+          <button @click="callAction02">调用模块中Actions中的方法 方式2</button>
+        </a-spin>
+        <a-divider/>
     </div>
 </template>
 
@@ -22,21 +36,42 @@ export default {
 
     ...mapState({
       // name: state => state.moduleC.text
-      name3: state => (state.moduleC.text + '和' + state.moduleD.text)
+      name3: state => (state.moduleC.text + '和' + state.moduleD.text),
+      //将全局的moduleF 赋值 给 state, 然后可以用this.state.xxx来获取其中的值
+      state: state => state.moduleF
     })
 
   },
 
   methods: {
     ...mapMutations({
-      m1: 'moduleF/setText'
+      m1: 'moduleF/setText',
+      setData : 'moduleF/setData'
     }),
     ...mapActions({
-      m2: 'moduleF/callAction'
+      m2: 'moduleF/callAction',
+      syncMethod:'moduleF/syncMethod'
     }),
     modifyNameAction () {
       this.m1()
       this.m2()
+    },
+    callMutations01(){
+      console.log(this.$store)
+      this.setData({user:{userName:'A1',age:1}})
+      //this.$store.dispatch("moduleF/syncMethod",{user:{userName:'A1',age:5}})
+    },
+    callMutations02(){
+      console.log(this.$store)
+      this.$store.commit("moduleF/setData",{user:{userName:'A2',age:2}})
+    },
+
+    callAction01(){
+      this.$store.dispatch("moduleF/syncMethod",{userName:'A3',age:3})
+    },
+    callAction02(){
+      this.syncMethod({userName:'A4',age:4});
+      
     }
   }
 }
--- src/modules/moduleF.js
+++ src/modules/moduleF.js
@@ -1,7 +1,14 @@
+import {message} from 'ant-design-vue'
 export default {
   namespaced: true,
   state: {
-    text: 'moduleF'
+    text: 'moduleF',
+    user: {
+      userName: 'devil13th',
+      age: 5,
+      birthday: '2018-01-01'
+    },
+    loading: false
   },
   getters: {
     // 注意:rootState必须是第三个参数
@@ -9,12 +16,28 @@ export default {
       return state.text + '-' + rootState.name + '  xxxxx'
     }
   },
+  // 用于处理异步或逻辑运算,如果要修改状态则要调用mutations中的方法
   actions: {
     callAction ({state, rootState}) {
       alert('moduleF:' + state.text + '-' + rootState.name)
+    },
+    syncMethod (context, parameter) {
+      // 调用mutation中的方法
+      context.commit('setData', {loading: true})
+
+      setTimeout(function () {
+        context.commit('setData', {user: parameter, loading: false})
+        message.info('success')
+      }, 1000)
     }
   },
+  // 用于修改state
   mutations: {
+    setData (state, payload) {
+      Object.keys(payload).forEach(element => {
+        state[element] = payload[element]
+      })
+    },
     setText (state) {
       state.text = 'F'
     }
